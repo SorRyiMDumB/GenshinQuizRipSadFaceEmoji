b131d0a258ae8434883b7f7716ef0854d1123c97
SorRyiMDumB <ahmadyyaseen12@gmail.com>
2022-04-29 09:50:37 +1000

    database fixed
    fied the tdta base script and hioi got jusst intaliszet the database and start work on my algo hopefull i wont wanna tear my eyes

--- databaseInit.py
+++ databaseInit.py
@@ -1,8 +1,8 @@
 import sqlite3
 from sqlite3 import Error
-from tkinter.messagebox import YES
 from userdata import userdataraw
 
+'''students contains the table columns '''
 students  = """
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,18 +23,25 @@ CREATE TABLE IF NOT EXISTS users (
 """
 
 def create_connection(path):
+    '''
+        Creates a connection to a database named data.sqlite, if not found a new database is created 
+    '''
     global connection
     connection = None
     try:
         connection = sqlite3.connect(path)
         print("Connection to SQLite DB successful")
         print("\n\nConnection to SQLite DB successful.", "\nDatabase: ", path)
+
     except Error as e:
         print(f"The error '{e}' occurred")
 
     return connection
 
 def execute_query(connection, query):
+    '''
+        Function takes in a connection and a query and places it in the connection
+    '''
     cursor = connection.cursor()
     try:
         cursor.execute(query)
@@ -43,17 +50,6 @@ def execute_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-def delete_all_tasks(conn):
-    """
-    Delete all rows in the tasks table
-    :param conn: Connection to the SQLite database
-    :return:
-    """
-    sql = 'DELETE FROM '
-    cur = conn.cursor()
-    cur.execute(sql)
-    conn.commit()
-
 def execute_read_query(connection, query):
     cursor = connection.cursor()
     result = None
@@ -64,24 +60,17 @@ def execute_read_query(connection, query):
     except Error as e:
         print(f"The error '{e}' occurred")
 
-def execute_read_query(connection, query):
-    cursor = connection.cursor()
-    result = None
-    try:
-        cursor.execute(query)
-        result = cursor.fetchall()
-        return result
-    except Error as e:
-        print(f"The error '{e}' occurred")
+
 
 create_connection("data.sqlite")
+cursor = connection.cursor()
+cursor.execute("DROP TABLE users")
 execute_query(connection, students)
 execute_query(connection, userdataraw)   
 
+
+
 select_users = "SELECT * from users"
 yes = execute_read_query(connection, select_users)
-
 for i in yes:
     print(i)
-
-#delete_all_tasks(connection)
