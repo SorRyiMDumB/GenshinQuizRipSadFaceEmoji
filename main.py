17c8197f25c03a3d44f002493c2ef2cd56aa71e1
SorRyiMDumB <ahmadyyaseen12@gmail.com>
2022-05-13 10:06:14 +1000

    AAA

--- main.py
+++ main.py
@@ -36,16 +36,16 @@ def addnodes():
         i[0], 
         year = i[1],                # shape /////
         house = i[2],               # edge, color, outline /////
-        classes = i[3],             # edge /////
+        classes = i[3],             # edge 
         genshin = i[4],                 # edge, color, outline /////
         uni = i[5],                 # edge /////
-        course = i[6],              # edge ///// 
-        f1 = i[7],              # edge, shape outline ///// 
-        sport = i[8],               # edge, color outline ///// 
-        tutorMath = i[9],               # edge ///// 
-        tutorEng = i[10],               # edge ///// 
-        tutorHums = i[11],              # edge ///// 
-        tutorScience = i[12],               # edge ///// 
+        course = i[6],              # edge /////
+        f1 = i[7],              # edge, shape outline /////
+        sport = i[8],               # edge, color outline /////
+        tutorMath = i[9],               # edge 
+        tutorEng = i[10],               # edge 
+        tutorHums = i[11],              # edge 
+        tutorScience = i[12],               # edge 
         friends = i[13],                 # size
         
         colour = "white",
@@ -54,7 +54,32 @@ def addnodes():
         outline = "black"
         )
 
-def connector_adv(param : str, colors : list, weights : list, labels : list):
+
+### CREATORS
+def node_con_exist(param,given_colour,given_weight):
+    """Connects all nodes where data is present in the param given.
+
+    ### Args:
+        param (string): connects if param is true
+        given_colour (string): what the color of node should be
+        given_weight (float): what the wieght of the node should be
+    """
+
+    worklist = []               # creates a working list
+
+    for node, data in G.nodes(data=True): # checks if data param is present in the graph
+        if data[param] != "0":              # checks if the data exists 
+            worklist.append(node)               #adds node to working list
+
+    while len(worklist) != 0:               # runs until the list is empty
+        for i in worklist:
+            if i == worklist[0]:
+                pass
+            else:
+                G.add_edge(i,worklist[0],colour= given_colour,weight= given_weight)             # connects nodes in working list with atributes
+        worklist.pop(0)
+
+def conn_test(param : str, colors : list, weights : list, labels : list):
     """ Connects nodes that share the same parameter
 
     Args:
@@ -81,6 +106,8 @@ def connector_adv(param : str, colors : list, weights : list, labels : list):
             uniqueVal.append(nodedata)
     
     # prints out statement saying what the unique values are in the parameter after all nodes have been searched
+    if "0" in uniqueVal:
+        uniqueVal.remove("0")
     print("#####################\nThe unique values in parameter: ",param, " are ", uniqueVal)
 
     # creates a simple counter to go through the lists provided to make sure the proper data is added to edges
@@ -131,58 +158,6 @@ def connector_adv(param : str, colors : list, weights : list, labels : list):
         
         # adds one to list counter so next time algo runs it uses next data
         listcounter = listcounter + 1
-
-
-
-### CREATORS
-def node_con_exist(param,given_colour,given_weight):
-    """Connects all nodes where data is present in the param given.
-
-    ### Args:
-        param (string): connects if param is true
-        given_colour (string): what the color of node should be
-        given_weight (float): what the wieght of the node should be
-    """
-
-    worklist = []               # creates a working list
-
-    for node, data in G.nodes(data=True): # checks if data param is present in the graph
-        if data[param] != "0":              # checks if the data exists 
-            worklist.append(node)               #adds node to working list
-
-    while len(worklist) != 0:               # runs until the list is empty
-        for i in worklist:
-            if i == worklist[0]:
-                pass
-            else:
-                G.add_edge(i,worklist[0],colour= given_colour,weight= given_weight)             # connects nodes in working list with atributes
-        worklist.pop(0)
-
-def node_con_test(param,testcase,given_colour,given_weight):
-    ''' 
-    Connects all nodes where data is equal to test case given.
-
-    ### EXAMPLE USAGE 
-    All nodes with param = food and testcase = burger will be connected
-        and given atributes colour = given colur and weight = given_weight
-    '''
-
-    worklist = []               # creates a working list
-
-    for node, data in G.nodes(data=True):               # checks if data param is present in the graph
-        if data[param] == testcase:             # checks if the data retrieved is equal to test case
-            worklist.append(node)               # adds node to working list
-        print("List that satisfy", testcase, "are: ", worklist)
-    while len(worklist) != 0:               # runs until the list is empty 
-        for i in worklist:
-            if i == worklist[0]:                # if the list is empty the function is ended 
-                pass
-            else: 
-                if type(given_weight) == str:
-                    G.add_edge(i,worklist[0],colour= given_colour,label= given_weight)
-                else:
-                    G.add_edge(i,worklist[0],colour= given_colour,weight= given_weight)             # creates edges to all nodes and adds atributes
-        worklist.pop(0)
 
 def attributor(param,testcase,atri,atri_data):
     '''
@@ -291,15 +266,6 @@ def sport_node(type, color : list):
     print(color)
 
 ### EDGE GENERATION
-def house_edge():
-    '''
-    Draws graph where all student in same house are connected.
-    '''
-
-    node_con_test("house","R","yellow","1")
-    node_con_test("house","B","blue","1")
-    node_con_test("house","C","green","1")
-    node_con_test("house","K","red","1")
 
 def classes_edge(given_colour):
     for node in G:
@@ -318,32 +284,6 @@ def classes_edge(given_colour):
                 else:
                     G.add_edge(node,testnode,colour= given_colour,weight= len(a))
 
-def genshin_edge():
-    '''
-    Draws graph where all student play genshin are connected.
-    '''
-
-    node_con_exist("genshin","pink","1")
-
-def uni_edge():
-    '''
-    Draws graph where all student who want the same uni are connected.
-    '''
-    node_con_test("uni","MEL", "purple","1")
-    node_con_test("uni","MON", "blue", "1")
-    node_con_test("uni","RMIT", "red", "1")
-
-def course_edge():
-    '''
-    Draws graph where all student who want the same course are connected.
-    '''
-    node_con_test("course","COM", "blue","Commerce")
-    node_con_test("course","FIN", "green","Finance")
-    node_con_test("course","MED", "red","Medicine")
-    node_con_test("course","ENG", "black","Engineering")
-    node_con_test("course","ART", "pink","Art")
-    node_con_test("course","LAW", "yellow","Law")
-
 
 def f1_edge():
     '''
@@ -352,23 +292,6 @@ def f1_edge():
 
     node_con_exist("f1","red","1")
 
-def sport_edge():
-
-    node_con_test("sport","BBALL", "yellow","Basketball")
-    node_con_test("sport","SWIM", "blue","Swimming")
-    node_con_test("sport","CRICKET", "green","Cricket")
-    node_con_test("sport","MMA", "red","Mixed martial arts")
-    node_con_test("sport","NETBALL", "pink","Netball")
-    node_con_test("sport","TENNIS", "orange","Tennis")
-
-def tutor_edge():
-    '''
-    Draws graph where all student who do tutoring are connected.
-    '''
-    node_con_exist("tutorEng","pink","1")
-    node_con_exist("tutorMath","blue","1")
-    node_con_exist("tutorHums","red","1")
-    node_con_exist("tutorScience","green","1")
 
 
 ### INIT
@@ -378,14 +301,25 @@ init()
 
 #house_node("node_color")
 
-#classes_edge("black")
+classes_edge("black")
 #sport_node("outline",["yellow","blue", "green", "red", "gold", "lime"])
 #sport_node("colour", ["pink","pink", "pink", "pink", "pink", "pink"])
 #sport_edge()
 #course_edge()
 
-connector_adv('house', ["blue", "yellow", "green", "red"], [1, 1, 1, 1], ["Blackwood", "Rothwell", "Cotrell", "Kororoit"])
-connector_adv('year', ["purple", "pink", "green", "red"], [1, 1, 1, 1], ["12", "11", "Cotrell", "Kororoit"])
+#conn_test('year', ["purple", "pink"], [1, 1], ["12", "11"])
+
+#conn_test('house', ["blue", "yellow", "green", "red"], [1, 1, 1, 1], ["Blackwood", "Rothwell", "Cotrell", "Kororoit"])
+
+#conn_test('genshin', ["red", "green", "light green", "blue", "dark blue", "grey", "purple"], [1, 1, 1, 1, 1, 1, 1], ['Hutao', 'Venti', 'Sucrose', 'Yelan ', 'Raiden Shogun', 'Razor', 'Lisa'])
+
+#conn_test('uni', ["blue", "turquoise", "yellow", "red", "green"], [1, 1, 1, 1, 1], ['Melbourne University', 'Monash University', 'Utrecht University', 'Royal Melbourne Institute of Technology', 'Victorian University'])
+
+#conn_test('course', ["grey", "green", "red", "yellow", "black", "pink", "brown", "gold","blue","orange"], [1,1,1,1,1,1,1,1,1,1,1], ['COM', 'FIN', 'MED', 'LIT', 'ENG', 'ART', 'PSY', 'LAW', 'SCI', 'MUS'] ) 
+
+#conn_test('f1', ["orange", "blue", "yellow", "light blue", "green", "red"], [1, 1, 1, 1, 1, 1], ['MCLAREN', 'RED BULL', 'HAAS', 'WILLIAMS', 'ALFA ROMEO', 'FERRARI'])
+
+#conn_test('sport', ["yellow","blue", "green", "red", "gold", "lime"], [1, 1,1,1,1,1,1,1,1], ['BBALL', 'SWIM', 'CRICKET', 'MMA', 'NETBALL', 'TENNIS'])
 
 ### GRAPH DRAWING
 def draw_graph(cheese):
@@ -395,11 +329,13 @@ def draw_graph(cheese):
     # drawing nodes
     for node in G:
         worklist = [node]
+        x = int(G.nodes[node]['weight'])
+        sizeX = (0.02 * ((x) ** 2)) + (8.6 * x) + 411.1
         nx.draw_networkx_nodes(
             G,
             pos,
             nodelist = worklist,
-            node_size = (int(G.nodes[node]['weight']) * 500),
+            node_size = float(sizeX),
             node_color = G.nodes[node]['colour'],
             node_shape = G.nodes[node]["shape"],
             edgecolors = G.nodes[node]["outline"],
@@ -421,17 +357,19 @@ def draw_graph(cheese):
         edge_color=edgecolor
     )
 
-    if cheese == "weight":
+    if cheese == "weighted":
         edgelabels = {}
         for n1, n2, data in G.edges.data():
             edgelabels[(n1, n2)] = data['weight']
+            print(n1,n2,data['weight'])
         nx.draw_networkx_edge_labels(
             G,
             pos,
             edge_labels=edgelabels,
             font_size=7
         )
-    else:
+
+    if cheese == "labeled":
         edgelabels = {}
         for n1, n2, data in G.edges.data():
             edgelabels[(n1, n2)] = data['label']
@@ -442,8 +380,11 @@ def draw_graph(cheese):
             font_size=7
         )
 
-    #print(G.nodes[11])
+    else:
+        pass
 
     plt.show()
 #raw_graph("weight")
-draw_graph('a')
+
+draw_graph('weighted')
+print("\n")
